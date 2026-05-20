7a79fec295255acff9c6e7d2159f828eaf2efa92
Salem Ba-Rabuod <barboud@gmail.com>
2026-05-20 13:46:53 +0200

    implement all model logic

--- task-1/com/pizzacompany/model/Order.java
+++ task-1/com/pizzacompany/model/Order.java
@@ -1,4 +1,35 @@
-package com.pizzacompany;
+package com.pizzacompany.model;
 
-public enum Order {
+import java.util.ArrayList;
+import java.util.List;
+
+public class Order {
+    List<Pizza> pizzas;
+
+    public Order() {
+        this.pizzas = new ArrayList<>();
+    }
+
+    public void addPizza(Pizza pizza) {
+        pizzas.add(pizza);
+    }
+
+    public double getTotalPrice() {
+        double total = 0;
+        for (Pizza pizza : pizzas) {
+            total += pizza.getTotalPrice();
+        }
+        return total;
+    }
+
+    public void printReceipt(){
+        System.out.println("=== Receipt ===");
+
+        for (Pizza pizza : pizzas) {
+            System.out.println(pizza);
+        }
+
+        System.out.println("---------------");
+        System.out.println("Total: €" + String.format("€%.2f", getTotalPrice()));
+    }
 }
--- task-1/com/pizzacompany/model/Pizza.java
+++ task-1/com/pizzacompany/model/Pizza.java
@@ -1,4 +1,41 @@
-package com.pizzacompany;
+package com.pizzacompany.model;
+
+import com.pizzacompany.enums.Size;
+import com.pizzacompany.enums.Topping;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Pizza {
+    private final Size size;
+    private List<Topping> toppingList = new ArrayList<>();
+
+    public Pizza(Size size, List<Topping> toppingList) {
+        this.size = size;
+        this.toppingList = toppingList;
+    }
+
+    public List<Topping> getToppingList() {
+        return toppingList;
+    }
+
+    public Size getSize() {
+        return size;
+    }
+
+    public double getTotalPrice(){
+        double totalPrice = 0;
+        for (Topping topping : toppingList) {
+            totalPrice += topping.getPrice();
+        }
+        totalPrice += size.getBasePrice();
+        return totalPrice;
+    }
+
+    @Override
+    public String toString() {
+        // MEDIUM pizza with [CHEESE, MUSHROOMS] - €11.50
+        return  size + " pizza with " + toppingList + " - €" + String.format(" - €%.2f", getTotalPrice());
+    }
+
 }
