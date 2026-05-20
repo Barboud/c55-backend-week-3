aab58d3252ecd478e2b6f818f24f7a3bc919bacc
Salem Ba-Rabuod <barboud@gmail.com>
2026-05-20 23:02:56 +0200

    implement Cash Class

--- task-2/src/main/java/org/my/cache/Cache.java
+++ task-2/src/main/java/org/my/cache/Cache.java
@@ -1,24 +1,30 @@
 package org.my.cache;
 
+import java.util.HashMap;
+import java.util.Map;
+
 class Cache<T> {
+
+  private Map<String, T> cash = new HashMap<>();
+
   public void put(String key, T value) {
-    throw new UnsupportedOperationException("Not implemented yet");
+    cash.put(key, value);
   }
 
   public T get(String key) {
-    throw new UnsupportedOperationException("Not implemented yet");
+    return cash.get(key);
   }
 
   public T remove(String key) {
-    throw new UnsupportedOperationException("Not implemented yet");
+    return cash.remove(key);
   }
 
   public int size() {
-    throw new UnsupportedOperationException("Not implemented yet");
+      return cash.size();
   }
 
   public void clear() {
-    throw new UnsupportedOperationException("Not implemented yet");
+    cash.clear();
   }
 
 }
--- task-2/src/main/java/org/my/cache/Main.java
+++ task-2/src/main/java/org/my/cache/Main.java
@@ -2,6 +2,17 @@ package org.my.cache;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello Cache!");
+        Cache<String> stringCache = new Cache<String>();
+        stringCache.put("name", "HackYourFuture");
+        System.out.println(stringCache.get("name")); // -> "HackYourFuture"
+        System.out.println(stringCache.get("city")); // -> null
+
+        Cache<Integer> numberCache = new Cache<Integer>();
+        numberCache.put("c1", 12345);
+        numberCache.put("c2", 54321);
+        numberCache.put("c3", 54321);
+        System.out.println(numberCache.size()); // -> 2
+        numberCache.remove("c1");
+        System.out.println(numberCache.get("c1")); // -> null
     }
 }
