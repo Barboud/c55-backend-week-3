3863b6f127f50a32e22538ae49be2e61dd38c9fa
Salem Ba-Rabuod <barboud@gmail.com>
2026-05-20 13:50:16 +0200

    test & complete task1

--- task-1/com/pizzacompany/Main.java
+++ task-1/com/pizzacompany/Main.java
@@ -1,7 +1,39 @@
 package com.pizzacompany;
 
+
+import com.pizzacompany.enums.Size;
+import com.pizzacompany.enums.Topping;
+import com.pizzacompany.model.Order;
+import com.pizzacompany.model.Pizza;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello Pizza!");
+        Order order = new Order();
+
+        List<Topping> toppingsFirstPizza = new ArrayList<>();
+        toppingsFirstPizza.add(Topping.CHEESE);
+        toppingsFirstPizza.add(Topping.MUSHROOMS);
+
+        Pizza pizza1 = new Pizza(Size.MEDIUM, toppingsFirstPizza);
+        order.addPizza(pizza1);
+
+        List<Topping> toppingsSecondPizza = new ArrayList<>();
+        toppingsSecondPizza.add(Topping.PINEAPPLE);
+        toppingsSecondPizza.add(Topping.OLIVES);
+        toppingsSecondPizza.add(Topping.CHEESE);
+
+        Pizza pizza2 = new Pizza(Size.LARGE, toppingsSecondPizza);
+        order.addPizza(pizza2);
+
+        List<Topping> toppingsThirdPizza = new ArrayList<>();
+        toppingsThirdPizza.add(Topping.PINEAPPLE);
+
+        Pizza pizza3 = new Pizza(Size.SMALL, toppingsThirdPizza);
+        order.addPizza(pizza3);
+
+        order.printReceipt();
     }
 }
